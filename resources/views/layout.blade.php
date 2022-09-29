9576d8fc034ee793ce59100506864c1d7ee2dce5
John Pevien Laguindam <johnpevienwork@gmail.com>
2022-09-29 22:07:17 +0800

    remove js code for nav
    migrate to alpinejs logic instead

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,8 @@
   <link href={{ asset('css/app.css') }} rel="stylesheet">
 </head>
 <body>
-  <header id="nav" class="sticky top-0 bg-transparent  flex flex-row  px-8 py-3 items-center justify-between transition-all transition-duration-200">
+  <header class="sticky top-0 bg-transparent  flex flex-row  px-8 py-3 items-center justify-between transition-all transition-duration-200"
+  :class="{ 'bg-white backdrop-blur shadow-md' : !top }"  @scroll.window="top = window.pageYOffset > 100 ? false : true" x-data="{ top: true }">
     <div class="h-full pt-2">
       <img class="w-24" src="{{ url('/site-logo.png') }}" alt="site logo">
     </div>
@@ -18,7 +19,7 @@
       </ul>
     </nav>
     <nav>
-      <ul class="flex flex-row gap-5 text-slate-700 items-center">
+      <ul class="flex flex-row gap-3 text-slate-700 items-center">
        
         <li><a class="button-primary" href="/register">Register</a></li>
         <li><a class="button-primary" href="/login">Login</a></li>
@@ -27,9 +28,10 @@
     </nav>
   </header>
   <main class="mt-10 container">
-    @yield('content');
+    @yield('content')
   </main>
   <script src="//unpkg.com/alpinejs" defer></script>
   <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
+
